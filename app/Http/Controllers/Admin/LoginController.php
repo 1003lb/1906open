843c8642f103bdd1b0faf776186f61b8ad890021
liu <liu@localhost.localdomain>
2020-02-27 06:12:18 +0000

    APPID+app_secret

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,17 +5,40 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\Login;
-
+use App\Model\AppModel;
 	class LoginController extends Controller{
-    //注册
+    //用户注册
     public function doreg(){
-    $post=request()->except('_token');
-    //dd($post);
-  $res=Login::insert($post);
-  //dd($res);
+      
+        //为用户生成AppID与secret
+    $name=request()->input('name');
+      $post=request()->except('_token');
+    //dump($post);
+   $res=Login::insert($post);
+    //dd($res);
+  //写入APP表中
+
   if($res){
-    echo "<script>alert('注册成功');location.href='/login'</script>";
+    echo "<script>alert('注册成功');</script>";
   }
+
+   $app_id=Login::Appid($name);
+    $app_secret=Login::Secret();
+  //写入APP表中
+    $app_info=[
+      'id' => $id,
+      'apps_id'=> $apps_id,
+      'apps_secret'=> $apps_secret,
+    ];
+    $app_id=AppModel::insertGetId($app_info);
+    if($app_id>0){
+          echo "ok";
+        }else{
+          echo "内部错误请联系管理员";
+      }
+    
+  echo "用户APPID：".$app_id;echo "<hr>";
+  echo "app_secret：".$app_secret;echo "<hr>";
     }
     //登录
 		public function dologin(){
--- app/Model/Login.php
+++ app/Model/Login.php
@@ -3,13 +3,23 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Str;
 class Login extends Model
 {
      public $primaryKey='id';
    public $table='admin';
    public $timestamps=false;
 	//黑名单 表设计中允许为空的
-   protected $guarded = [];	
+   //protected $guarded = [];	
    
+   //生成AppID 规则 根据用户名+时间戳 +随机数 进行MD5
+   public static function Appid($name){
+
+    return 'ln'. substr(md5($name.time() . mt_rand(111111,999999)),5,16);
+
+   }
+   //生成APP secret
+   public static function Secret(){
+   	return Str::random(32);
+   }
 }
